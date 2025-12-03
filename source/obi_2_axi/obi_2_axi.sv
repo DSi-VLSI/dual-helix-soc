1d3ac243db7943dee2c6128421a4c5c9a643e765
S. M. Tahmeed Reza <tahmeed.reza@dsinnovators.com>
2025-12-03 12:29:58 +0600

    modified:   source/obi_2_axi/obi_2_axi.sv new file:   source/obi_2_axi/obi_2_axi_core.sv

--- source/obi_2_axi/obi_2_axi.sv
+++ source/obi_2_axi/obi_2_axi.sv
@@ -1,23 +1,33 @@
-
 module obi_2_axi #(
+    parameter int OBI_FIFO_DEPTH = 4,
+    parameter int AXI_FIFO_DEPTH = 4,
+
     parameter int OBI_ADDRW = 32,  // Address width
     parameter int OBI_DATAW = 32,  // Data Width
     parameter int OBI_STRBW = (OBI_DATAW / 8),  // Strobe width
+
+    parameter type aw_chan_t = logic,
+    parameter type w_chan_t  = logic,
+    parameter type b_chan_t  = logic,
+    parameter type ar_chan_t = logic,
+    parameter type r_chan_t  = logic,
 
     parameter type axi_req_t  = logic,  // AXI Request structure
     parameter type axi_resp_t = logic   // AXI Response structure
 ) (
 
-    input logic clk_i,   // Core speed
-    input logic arst_ni, // Async Global Reset
+    input logic clk_obi_i,  // OBI Clock
+    input logic clk_axi_i,  // AXI clock
+    input logic arst_ni,    // Async Global Reset
 
-    input  logic                 req_i,     // OBI Request
-    output logic                 gnt_o,     // OBI Grant
+    input  logic [OBI_ADDRW-1:0] addr_i,   // OBI request address
+    input  logic                 we_i,     // OBI write enable
+    input  logic [OBI_DATAW-1:0] wdata_i,  // OBI write data
+    input  logic [OBI_STRBW-1:0] be_i,     // OBI byte enable
+    input  logic                 req_i,    // OBI Request
+    output logic                 gnt_o,    // OBI Grant
+
     output logic                 rvalid_o,  // OBI rvalid
-    input  logic                 we_i,      // OBI write enable
-    input  logic [OBI_STRBW-1:0] be_i,      // OBI byte enable
-    input  logic [OBI_ADDRW-1:0] addr_i,    // OBI request address
-    input  logic [OBI_DATAW-1:0] wdata_i,   // OBI write data
     output logic [OBI_DATAW-1:0] rdata_o,   // OBI read data
 
     output axi_req_t  axi_req_o,  // AXI request signals
@@ -25,92 +35,89 @@ module obi_2_axi #(
 
 );
 
-  typedef enum {
-    IDLE,
-    SEND_AR,
-    RECV_R,
-    SEND_AW,
-    SEND_W,
-    RECV_B,
-    GRANT
-  } axi_fsm_state_e;
+  logic      [OBI_ADDRW-1:0] intr_addr_i;
+  logic                      intr_we_i;
+  logic      [OBI_DATAW-1:0] intr_wdata_i;
+  logic      [OBI_STRBW-1:0] intr_be_i;
+  logic                      intr_req_i;
+  logic                      intr_gnt_o;
 
-  axi_fsm_state_e axi_fsm_state, axi_fsm_next_state;
+  logic                      intr_rvalid_o;
+  logic      [OBI_DATAW-1:0] intr_rdata_o;
 
-  always_comb begin
-    axi_req_o = '0;
-    axi_req_o.aw.addr = addr_i;
-    axi_req_o.aw.size = 2;
-    axi_req_o.aw.burst = 1;
+  axi_req_t                  intr_axi_req_o;
+  axi_resp_t                 intr_axi_resp_i;
 
-    axi_req_o.ar.addr = addr_i;
-    axi_req_o.ar.size = 2;
-    axi_req_o.ar.burst = 1;
+  cdc_fifo #(
+      .ELEM_WIDTH($bits(intr_addr_i) + $bits(intr_we_i) + $bits(intr_wdata_i) + $bits(intr_be_i)),
+      .FIFO_SIZE ($clog2(OBI_FIFO_DEPTH))
+  ) u_req_fifo (
+      .arst_ni(arst_ni),
+      .elem_in_i({addr_i, we_i, wdata_i, be_i}),
+      .elem_in_clk_i(clk_obi_i),
+      .elem_in_valid_i(req_i),
+      .elem_in_ready_o(gnt_o),
+      .elem_out_o({intr_addr_i, intr_we_i, intr_wdata_i, intr_be_i}),
+      .elem_out_clk_i(clk_axi_i),
+      .elem_out_valid_o(intr_req_i),
+      .elem_out_ready_i(intr_gnt_o)
+  );
 
-    axi_req_o.w.data = wdata_i;
-    axi_req_o.w.strb = be_i;
-    axi_req_o.w.last = '1;
+  cdc_fifo #(
+      .ELEM_WIDTH($bits(intr_rdata_o)),
+      .FIFO_SIZE ($clog2(OBI_FIFO_DEPTH))
+  ) u_resp_fifo (
+      .arst_ni(arst_ni),
+      .elem_in_i(intr_rdata_o),
+      .elem_in_clk_i(clk_axi_i),
+      .elem_in_valid_i(intr_rvalid_o),
+      .elem_in_ready_o(),
+      .elem_out_o(rdata_o),
+      .elem_out_clk_i(clk_obi_i),
+      .elem_out_valid_o(rvalid_o),
+      .elem_out_ready_i('1)
+  );
 
-    rdata_o = axi_resp_i.r.data;
 
-    gnt_o = 0;
-    rvalid_o = '0;
+  axi_fifo #(
+      .Depth      (AXI_FIFO_DEPTH),
+      .FallThrough('0),
+      .aw_chan_t  (aw_chan_t),
+      .w_chan_t   (w_chan_t),
+      .b_chan_t   (b_chan_t),
+      .ar_chan_t  (ar_chan_t),
+      .r_chan_t   (r_chan_t),
+      .axi_req_t  (axi_req_t),
+      .axi_resp_t (axi_resp_t)
+  ) u_axi_fifo (
+      .clk_i(clk_axi_i),
+      .rst_ni(arst_ni),
+      .test_i('0),
+      .slv_req_i(intr_axi_req_o),
+      .slv_resp_o(intr_axi_resp_i),
+      .mst_req_o(axi_req_o),
+      .mst_resp_i(axi_resp_i)
+  );
 
-    axi_req_o.ar_valid = 0;
-    axi_req_o.r_ready = 0;
-
-    axi_req_o.aw_valid = 1'b0;
-    axi_req_o.w_valid = 1'b0;
-    axi_req_o.ar_valid = 1'b0;
-
-    case (axi_fsm_state)
-      IDLE: begin
-        if (req_i) axi_fsm_next_state = SEND_AR;
-      end
-      SEND_AR: begin
-        axi_req_o.ar_valid = '1;
-        if (axi_resp_i.ar_ready) axi_fsm_next_state = RECV_R;
-      end
-      RECV_R: begin
-        if (axi_resp_i.r_valid && axi_resp_i.r.last) begin
-          if (we_i) begin
-            axi_fsm_next_state = SEND_AW;
-          end else begin
-            axi_fsm_next_state = GRANT;
-          end
-        end
-      end
-      SEND_AW: begin
-        axi_req_o.aw_valid = '1;
-        if (axi_resp_i.aw_ready) axi_fsm_next_state = SEND_W;
-      end
-      SEND_W: begin
-        axi_req_o.w_valid = '1;
-        if (axi_resp_i.w_ready && axi_resp_i.w.last) axi_fsm_next_state = RECV_B;
-      end
-      RECV_B: begin
-        axi_req_o.b_ready = '1;
-        if (axi_resp_i.b_valid) begin
-          axi_fsm_next_state = GRANT;
-        end
-      end
-      GRANT: begin
-        axi_req_o.r_ready = '1;
-        gnt_o = '1;
-        rvalid_o = '1;
-        axi_fsm_next_state = IDLE;
-      end
-      default: begin
-      end
-    endcase
-  end
-
-  always_ff @(posedge clk_i or negedge arst_ni) begin
-    if (~arst_ni) begin
-      axi_fsm_state <= IDLE;
-    end else begin
-      axi_fsm_state <= axi_fsm_next_state;
-    end
-  end
+  obi_2_axi_core #(
+      .OBI_ADDRW (OBI_ADDRW),
+      .OBI_DATAW (OBI_DATAW),
+      .OBI_STRBW (OBI_STRBW),
+      .axi_req_t (axi_req_t),
+      .axi_resp_t(axi_resp_t)
+  ) u_core (
+      .clk_i(clk_axi_i),
+      .arst_ni(arst_ni),
+      .req_i(intr_req_i),
+      .gnt_o(intr_gnt_o),
+      .rvalid_o(intr_rvalid_o),
+      .we_i(intr_we_i),
+      .be_i(intr_be_i),
+      .addr_i(intr_addr_i),
+      .wdata_i(intr_wdata_i),
+      .rdata_o(intr_rdata_o),
+      .axi_req_o(intr_axi_req_o),
+      .axi_resp_i(intr_axi_resp_i)
+  );
 
 endmodule
