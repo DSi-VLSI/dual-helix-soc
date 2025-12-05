1f904cdb61c1b8bd93fabe21909f5dda5bbc6f1a
S. M. Tahmeed Reza <tahmeed.reza@dsinnovators.com>
2025-12-05 14:22:55 +0600

    modified:   Makefile modified:   hardware/filelist/axi.f modified:   hardware/filelist/dhs.f renamed:    hardware/source/apb_2_axil.sv -> hardware/source/apb_2_axil/apb_2_axil.sv

--- hardware/filelist/axi.f
+++ hardware/filelist/axi.f
@@ -3,6 +3,8 @@
 
 ${AXI_DIR}/src/axi_pkg.sv
 ${COMMON_CELLS_DIR}/src/cf_math_pkg.sv
+${SOC_DIR}/package/ariane_axi_pkg.sv
+${SOC_DIR}/package/soc_pkg.sv
 
 ${COMMON_CELLS_DIR}/src/stream_register.sv
 ${AXI_DIR}/src/axi_atop_filter.sv
@@ -13,7 +15,6 @@ ${AXI_DIR}/src/axi_demux_simple.sv
 ${COMMON_CELLS_DIR}/src/counter.sv
 ${COMMON_CELLS_DIR}/src/lzc.sv
 ${COMMON_CELLS_DIR}/src/rr_arb_tree.sv
-${AXI_DIR}/src/axi_demux_simple.sv
 ${AXI_DIR}/src/axi_demux.sv
 ${COMMON_CELLS_DIR}/src/fifo_v3.sv
 ${AXI_DIR}/src/axi_err_slv.sv
@@ -23,7 +24,8 @@ ${SOC_DIR}/source/axi_burst_splitter_counters.sv
 ${SOC_DIR}/source/axi_burst_splitter_ax_chan.sv
 ${AXI_DIR}/src/axi_burst_splitter.sv
 ${AXI_DIR}/src/axi_to_axi_lite.sv
-${AXI_DIR}/src/axi_to_axi_lite.sv
 ${AXI_DIR}/src/axi_fifo.sv
 ${AXI_DIR}/src/axi_xbar_unmuxed.sv
 ${AXI_DIR}/src/axi_xbar.sv
+${SOC_DIR}/source/axi_to_simple_if.sv
+${SOC_DIR}/source/axi_ram.sv
